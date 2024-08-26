fcd2e882e7b8164573cf91817fc4bdb9beb7e5e3
eileen819 <eileen.ju.8819@gmail.com>
2024-08-27 05:02:31 +0900

    Modifying css and Implementing D-DAY

---
+++ js/dday.js
@@ -0,0 +1,117 @@
+const ddayForm = document.querySelector("#dday-form");
+const ddayTitle = document.querySelector(".dday-title");
+const ddayDate = document.querySelector(".dday-date");
+const showFormBtn = document.querySelector(".show-form");
+const removeFormBtn = document.querySelector(".cancel-form");
+const errorMessage = document.querySelector(".error-message");
+const ddayList = document.querySelector(".dday-list");
+
+const D_DAY_KEY = "d-days";
+let ddays = [];
+
+function saveDday() {
+  localStorage.setItem(D_DAY_KEY, JSON.stringify(ddays));
+}
+
+function removeEl(event) {
+  const targetEl = event.target.parentElement;
+  targetEl.remove();
+  ddays = ddays.filter((dday) => dday.id !== parseInt(targetEl.id));
+  saveDday();
+}
+
+function paintDday(newDdayObj, li) {
+  const timeText = li.querySelector(".d-day_leftTime");
+  function countDays() {
+    const now = new Date().getTime();
+    const targetTime = new Date(newDdayObj.date).getTime();
+    const diff = targetTime - now;
+    const days = String(Math.floor(diff / (1000 * 60 * 60 * 24)));
+
+    if (diff > 0) {
+      timeText.innerText = `D-${days}`;
+    } else if (diff === 0) {
+      timeText.innerText = `오늘은 ${newDdayObj.title}의 날입니다.`;
+    } else {
+      timeText.innerText = `D+${Math.abs(days)}`;
+    }
+  }
+  countDays();
+  setInterval(countDays, 1000);
+}
+
+function addDday(newDdayObj) {
+  const li = document.createElement("li");
+  li.id = newDdayObj.id;
+
+  const spanTitle = document.createElement("span");
+  spanTitle.classList.add("d-day_title");
+  spanTitle.innerText = newDdayObj.title;
+
+  const spanLeftTime = document.createElement("span");
+  spanLeftTime.classList.add("d-day_leftTime");
+
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-dday");
+  removeBtn.innerText = "delete";
+  removeBtn.addEventListener("click", removeEl);
+
+  li.append(spanTitle, spanLeftTime, removeBtn);
+  ddayList.appendChild(li);
+
+  paintDday(newDdayObj, li);
+}
+
+function onSubmitDday(event) {
+  event.preventDefault();
+
+  const newTitle = ddayTitle.value;
+  const newDate = ddayDate.value;
+
+  if (
+    !newDate.match(/^([1-9]\d{3})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/g)
+  ) {
+    ddayDate.value = "";
+    errorMessage.innerText = "올바르게 날짜를 입력해주세요.\n예시) 2024-01-01";
+    return;
+  }
+  errorMessage.innerText = "";
+
+  const newDdayObj = {
+    id: Date.now(),
+    title: newTitle,
+    date: newDate,
+  };
+
+  ddays.push(newDdayObj);
+  saveDday();
+  addDday(newDdayObj);
+  ddayForm.reset();
+  ddayForm.classList.add("hidden");
+  showFormBtn.classList.remove("hidden");
+  removeFormBtn.classList.add("hidden");
+  ddayList.classList.remove("hidden");
+}
+
+showFormBtn.addEventListener("click", () => {
+  ddayForm.classList.remove("hidden");
+  showFormBtn.classList.add("hidden");
+  removeFormBtn.classList.remove("hidden");
+  ddayList.classList.add("hidden");
+});
+
+removeFormBtn.addEventListener("click", () => {
+  ddayForm.classList.add("hidden");
+  removeFormBtn.classList.add("hidden");
+  showFormBtn.classList.remove("hidden");
+  ddayList.classList.remove("hidden");
+});
+
+ddayForm.addEventListener("submit", onSubmitDday);
+
+const savedDdays = localStorage.getItem(D_DAY_KEY);
+if (savedDdays !== null) {
+  const parsedDdays = JSON.parse(savedDdays);
+  ddays = parsedDdays;
+  ddays.forEach(addDday);
+}
--- js/todo.js
+++ js/todo.js
@@ -5,63 +5,71 @@ const toDoList = document.querySelector("#todo-list");
 const TODOS_KEY = "todos";
 let toDos = [];
 
-function saveToDos() {
+function saveToDo() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function onDeleteToDo(event) {
-  const li = event.target.parentElement;
-  li.remove();
-  toDos = toDos.filter((todo) => todo.id !== parseInt(li.id)); // todo.id는 숫자, li.id는 문자이므로
-  saveToDos();
-  // 새로운 배열을 만들었기때문에 로컬스토리지를 업데이트해 줘야 함
+function removeToDo(event) {
+  const targetEl = event.target.parentElement;
+  targetEl.remove();
+  toDos = toDos.filter((todo) => todo.id !== parseInt(targetEl.id));
+  saveToDo();
 }
 
-function paintToDo(toDoObj) {
+function paintToDo(newToDoObj) {
   const li = document.createElement("li");
-  li.id = toDoObj.id;
-  const span = document.createElement("span");
-  span.innerText = toDoObj.text;
-  const button = document.createElement("button");
-  button.innerText = "❌";
-  button.addEventListener("click", onDeleteToDo);
-  li.appendChild(span);
-  li.append(button);
+  li.id = newToDoObj.id;
+
+  const div = document.createElement("div");
+  div.classList.add("todo-text");
+
+  const checkInput = document.createElement("input");
+  checkInput.setAttribute("type", "checkbox");
+  checkInput.id = `checkbox-${newToDoObj.id}`;
+  checkInput.checked = newToDoObj.completed;
+
+  const label = document.createElement("label");
+  label.setAttribute("for", `checkbox-${newToDoObj.id}`);
+  label.innerText = newToDoObj.todo;
+
+  if (checkInput.checked) {
+    label.classList.add("complete");
+  }
+
+  checkInput.addEventListener("click", () => {
+    newToDoObj.completed = checkInput.checked;
+    label.classList.toggle("complete");
+    saveToDo();
+  });
+
+  const removeBtn = document.createElement("button");
+  removeBtn.innerText = "delete";
+  removeBtn.addEventListener("click", removeToDo);
+
+  div.append(checkInput, label);
+  li.append(div, removeBtn);
   toDoList.appendChild(li);
 }
 
-function onToDoSubmit(event) {
+function addTodoList(event) {
   event.preventDefault();
-  const newToDo = toDoInputEl.value; // newToDo는 인풋요소의 value값을 복사한 것, 따라서 기존의 인풋값과 별개가 됨
-  toDoInputEl.value = ""; // newToDo로 인풋값을 복사해뒀기 때문에, 기존의 값을 비워줄 수 있음
+  const newToDo = toDoInputEl.value;
+  toDoInputEl.value = "";
   const newToDoObj = {
     id: Date.now(),
-    text: newToDo,
+    todo: newToDo,
+    completed: false,
   };
-  toDos.push(newToDoObj); // push메소드: 대상 배열의 원본이 변경됨
-  saveToDos();
+  toDos.push(newToDoObj);
+  saveToDo();
   paintToDo(newToDoObj);
 }
 
-toDoForm.addEventListener("submit", onToDoSubmit);
+toDoForm.addEventListener("submit", addTodoList);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
-  // toDos는 빈 배열로 정의되어져 있음.
-  // 따라서 새로고침을 하면(새롭게 어플리케이션 시작) 기존에 데이터가 있다해도
-  // 처음에 정의되어 있는 빈배열에 다시 push하게 됨으로 기존의 데이터가 삭제가 됨
-  // 이런 오류를 방지하고자, toDos의 배열이 변경될 때, 데이터가 있는 상태로 업데이트(재정의) 해주는 것임
-  // useEffect처럼 웹페이지 열리는 초기에 실행되는 것
   parsedToDos.forEach(paintToDo);
-  // forEach의 인수는 함수로 들어가야 하고,
-  // forEach의 인수로 들어간 함수의 인수는 forEach가 사용될 배열아이템을 알아서 넣어줄 것임
-  // (그렇게 되도록 만들어진 함수)
-  // 위의 코드를 작성하지 않으면 로컬스토리지에 데이터가 저장되어 있어도, 새롭게 추가하는 내용만 화면에 그려짐
-  // why? forEach메소드는 로컬스토리지에 저장되어있는 값(배열)으로 동작되기 때문(연속으로 동작되는 게 아님 1번만 동작됨)
 }
-
-// 위의 코드들은 함수를 정의한 것이 대부분이며
-// 실제 실행되는 코드는 submit했을 때 발생하는 이벤트 리스너와 새로고침 후에 처음에 if 조건 통과 여부로 그려지는 화면
-// 이렇게 두가지 뿐임을 기억하자
