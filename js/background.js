d560aad25568bca1301fd14098e2b4c5ac28de91
eileen819 <eileen.ju.8819@gmail.com>
2024-08-28 18:50:21 +0900

    반응형 작업진행

--- js/background.js
+++ js/background.js
@@ -24,14 +24,26 @@ const image = [
     URL: "https://images.unsplash.com/photo-1720832657295-68f54a7ed4e9?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    id: 7,
+    id: 6,
     URL: "https://images.unsplash.com/photo-1721519342375-b01de76a4e93?q=80&w=2675&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 7,
+    URL: "https://images.unsplash.com/photo-1724424188343-b5ab1cfe45b9?q=80&w=2908&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 8,
+    URL: "https://images.unsplash.com/photo-1724396344968-87e8cd5024e8?q=80&w=2675&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 9,
+    URL: "https://images.unsplash.com/photo-1724288897185-1298a057d8b3?q=80&w=2831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
 const randomImage = image[Math.floor(Math.random() * image.length)];
 
-document.body.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.6), rgba(255,255,255,0.6)), url(${randomImage.URL})`;
+document.body.style.backgroundImage = `linear-gradient(rgba(255,255,255,0.6), rgba(255,255,255,0.8)), url(${randomImage?.URL})`;
 document.body.style.backgroundSize = "cover";
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundAttachment = "fixed";
